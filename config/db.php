905e81d6a8ed3a17231ae638a528fdd1aabdb2a6
yhuda elsadai <yhudaelsadai@gmail.com>
2025-09-20 13:50:43 +0700

    Initial commit: EMS School Management Project

---
+++ config/db.php
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+<?php
+$host = 'localhost';
+$dbname = 'ems_school';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+=======
+<?php
+$host = 'localhost';
+$dbname = 'ems_school';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+>>>>>>> 14bbd93 (Inisialisasi proyek XAMPP)
+?>
